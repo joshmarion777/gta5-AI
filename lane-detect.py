b9fd1fb5604a1bea3f4dc7b49859b51c8acde640
joshua <joshmarion777@gmail.com>
2018-01-07 21:55:13 +0530

    Simple self driving functionality

--- lane-detect.py
+++ lane-detect.py
@@ -5,12 +5,30 @@ import time
 import pyautogui
 from numpy import ones,vstack
 from numpy.linalg import lstsq
-from directkey import ReleaseKey, PressKey, W, A, S, D 
+from directkey import PressKey,ReleaseKey, W, A, S, D
 from statistics import mean
 
+def roi(img, vertices):
+    
+    #blank mask:
+    mask = np.zeros_like(img)   
+    
+    #filling pixels inside the polygon defined by "vertices" with the fill color    
+    cv2.fillPoly(mask, vertices, 255)
+    
+    #returning the image only where mask pixels are nonzero
+    masked = cv2.bitwise_and(img, mask)
+    return masked
 
-def draw_lanes(img, lines):
+
+def draw_lanes(img, lines, color=[0, 255, 255], thickness=3):
+
+    # if this fails, go with some default line
     try:
+
+        # finds the maximum y value for a lane marker 
+        # (since we cannot assume the horizon will always be at the same point.)
+
         ys = []  
         for i in lines:
             for ii in i:
@@ -72,7 +90,7 @@ def draw_lanes(img, lines):
         lane1_id = top_lanes[0][0]
         lane2_id = top_lanes[1][0]
 
-         def average_lane(lane_data):
+        def average_lane(lane_data):
             x1s = []
             y1s = []
             x2s = []
@@ -87,30 +105,32 @@ def draw_lanes(img, lines):
         l1_x1, l1_y1, l1_x2, l1_y2 = average_lane(final_lanes[lane1_id])
         l2_x1, l2_y1, l2_x2, l2_y2 = average_lane(final_lanes[lane2_id])
 
-        return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2]
-     except Exception as e:
-            print(str(e))  
-               
-def  roi(img, vertices):
-    mask = np.zeros_like(img)
-    cv2.fillPoly(mask, vertices, 255)
-    masked = cv2.bitwise_and(img, mask)
-    return masked
+        return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2],lane1_id, lane2_id
+    except Exception as e:
+        print(str(e))
+
 
 def process_img(image):
- 
     original_image = image
-    #BGR TO GRAY because the [0,255,255] is bgr and [0,8,8] is gray for simpler data manipulation
-    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-    processed_img = cv2.Canny(processed_img, 200, 300)
-    processed_img = cv2.GaussianBlur(processed_img, (5,5), 0) 
-    vertices = np.array([[10,500], [10,300],[300,200],[500,200],[800,300],[800,500]], np.int32)
-    processed_img = roi(processed_img, [vertices])
+    # convert to gray
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # edge detection
+    processed_img =  cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
     
-    #       edges
-    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180,np.array([]), 100, 5)
+    processed_img = cv2.GaussianBlur(processed_img,(5,5),0)
+    
+    vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500],
+                         ], np.int32)
+
+    processed_img = roi(processed_img, [vertices])
+
+    # more info: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
+    #                                     rho   theta   thresh  min length, max gap:        
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180,      20,       15)
+    m1 = 0
+    m2 = 0 
     try:
-        l1, l2 = draw_lanes(original_image,lines)
+        l1, l2, m1, m2 =  draw_lanes(original_image,lines)
         cv2.line(original_image, (l1[0], l1[1]), (l1[2], l1[3]), [0,255,0], 30)
         cv2.line(original_image, (l2[0], l2[1]), (l2[2], l2[3]), [0,255,0], 30)
     except Exception as e:
@@ -128,21 +148,53 @@ def process_img(image):
     except Exception as e:
         pass
 
-    return processed_img, original_image
+    return processed_img,original_image, m1, m2
 
-def main():
+def straight():
+    PressKey(W)
+    ReleaseKey(A)
+    ReleaseKey(D)
+
+def left():
+    PressKey(A)
+    ReleaseKey(W)
+    ReleaseKey(D)
+    ReleaseKey(D)
+
+
+def right():
+    PressKey(D)
+    ReleaseKey(A)
+    ReleaseKey(W)
+    ReleaseKey(D)
+
+
+def slow_ya_roll():
+    ReleaseKey(W)
+    ReleaseKey(A)
+    ReleaseKey(D)
+
+for i in list(range(4))[::-1]:
+    print(i+1)
+    time.sleep(1)
+
+last_time = time.time()
+while True:
+    screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+    print('Frame took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
-    while(True):
-        # 800x600 windowed mode
-        screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
-        print('loop took {} seconds'.format(time.time()-last_time))
-        last_time = time.time()
-        new_screen,original_image = process_img(screen)
-        cv2.imshow('window', new_screen)
-        cv2.imshow('window2', cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
-       # cv2.imshow('window2',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
+    new_screen,original_image, m1, m2 = process_img(screen)
+    #cv2.imshow('window', new_screen)
+    cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+    
+    if m1 <0 and m2 < 0:
+        right()
+    elif m1 > 0 and m2 > 0:
+        left()
+    else:
+        straight()
 
-main()
+    #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
